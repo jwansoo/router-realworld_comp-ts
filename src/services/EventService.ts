3bed3e6395bbb55f769c7c2554da5f5e34e86b6c
wansoo <jwansoo@hanmail.net>
2022-12-31 10:29:58 +0900

    swnd

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/jwansoo/router_realworld_comp_ts',
+  baseURL: 'https://my-json-server.typicode.com/jwansoo/router_realworld_comp-ts',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
